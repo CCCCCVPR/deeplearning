5bbfd20dab7b43c01a39138e5f25fa68be192b9c
das135 <31717144+das135@users.noreply.github.com>
2017-09-12 03:59:34 +0900

    Update model.py

--- CSdoctor-Project/model.py
+++ CSdoctor-Project/model.py
@@ -13,6 +13,8 @@ from keras.utils import np_utils
 batch_size = 500
 epochs = 64
 
+
+
 # Data parsing
 data = pd.read_csv('E://capstone/data.csv', header = None)
 data = data.reindex(np.random.permutation(data.index))
@@ -20,14 +22,20 @@ train, test = train_test_split(data, train_size=0.9, random_state=0)
 x_train, x_test = train.iloc[:,1:].values, test.iloc[:,1:].values
 y_train, y_test = np_utils.to_categorical(train.iloc[:,0].values), np_utils.to_categorical(test.iloc[:,0].values)
 
+
+
 # input_shape = (none, 50000)
 input_shape = (x_train.shape[1], )
 output_shape = y_train.shape[1]
 
+
+
 # Model (3D-CNN + LSTM)
+# Model Start
 model = Sequential()
 model.add(Reshape(input_shape=input_shape, target_shape=(50, 10, 10, 10, 1)))
 
+# 3D-CNN
 model.add(TimeDistributed(Convolution3D(64, 3, 3, 3, border_mode='valid', activation='tanh')))
 model.add(TimeDistributed(Convolution3D(128, 3, 3, 3, border_mode='valid', activation='tanh')))
 model.add(TimeDistributed(Convolution3D(256, 3, 3, 3, border_mode='valid', activation='tanh')))
@@ -36,19 +44,33 @@ model.add(TimeDistributed(MaxPooling3D(pool_size=(2, 2, 2))))
 model.add(TimeDistributed(Activation('tanh')))
 model.add(TimeDistributed(Dropout(0.25)))
 model.add(TimeDistributed(Flatten()))
-model.add(TimeDistributed(Dense(128)))
-model.add(LSTM(50, return_sequences=False))
-model.add(Dense(output_shape))
+
+# Attention
+attention_model = Sequential()
+attention_model.add(model)
+attention_model.add(TimeDistributed(Dense(256, activation='softmax')))
+final_model = Sequential()
+final_model.add(Merge([model, attention_model], mode='mul'))
+
+# LSTM
+final_model.add(TimeDistributed(Dense(128)))
+final_model.add(LSTM(50, return_sequences=False))
+final_model.add(Dense(output_shape))
 
 model.summary()
+attention_model.summary()
+final_model.summary()
 
 
-model.compile(optimizer='adadelta',
+# Compile
+final_model.compile(optimizer='adadelta',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
+
+
 # Training Start
-history = model.fit(x_train, y_train,
+history = final_model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
@@ -56,6 +78,6 @@ history = model.fit(x_train, y_train,
 
 
 # Result
-score = model.evaluate(x_test, y_test, verbose=0)
+score = final_model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
